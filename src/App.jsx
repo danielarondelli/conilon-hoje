e3ecb6084944b6a3a869a668bc902985cbb0b506
danielarondelli <danielarondellidesousa@gmail.com>
2026-06-09 14:23:36 -0300

    Corrigir aparência do seletor do simulador

--- src/App.jsx
+++ src/App.jsx
@@ -495,7 +495,7 @@ top: 14,
     fontSize: 14,
     outline: "none",
     boxSizing: "border-box",
-    appearance: "none",
+    appearance: "auto",
   }}
 >
   <option value="7" style={{ background: "#1a1a1a" }}>Tipo 7</option>
