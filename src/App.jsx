e15bbaedccb583b3f50f1e823a4a94f9f7f2f2f2
danielarondelli <danielarondellidesousa@gmail.com>
2026-06-09 16:48:00 -0300

    Update totalVenda calculation with selectedType

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export default function App() {
   const [precoVenda, setPrecoVenda] = React.useState("");
   const [selectedType, setSelectedType] = React.useState("7");
   const totalVenda =
-  (Number(sacas) || 0) * (Number(precoVenda) || 0);
+  (Número(sacas) || 0) * (Número(precosPorTipo[selectedType] ?? pré-revestimento) || 0);
   const [clima, setClima] = React.useState([]);
   const [statusClima, setStatusClima] = React.useState("Buscando clima da sua região...");
 const [planilha,setPlanilha] = React.useState([])
